0825f5ee852a43a3eced863c37f83be1ea72f62d
BrianDuPont <briandupont87@gmail.com>
2018-08-07 11:22:12 -0400

    Add addPart and addParts to Asset model.

--- app/Asset.php
+++ app/Asset.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Part;
 use App\WorkOrder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 
 class Asset extends Model
 {
@@ -14,13 +15,57 @@ class Asset extends Model
         'description',
     ];
 
+    /**
+     * Get all Orders associated with the Asset.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function orders()
     {
         return $this->hasMany(WorkOrder::class);
     }
 
+    /**
+     * Get all Parts associated with the Asset.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function parts()
     {
         return $this->hasMany(Part::class);
+    }
+
+    /**
+     * Associate a Part with the Asset.
+     *
+     * @param  App\Part|Illuminate\Database\Eloquent\Collection  $part
+     * @return $this
+     */
+    public function addPart($part)
+    {
+        if ($part instanceof Collection) {
+            return $this->addParts($part);
+        }
+
+        $this->parts()->save($part);
+
+        return $this;
+    }
+
+    /**
+     * Associate a Collection of Parts with the Asset.
+     *
+     * @param  Illuminate\Database\Eloquent\Collection|App\Part  $parts
+     * @return $this
+     */
+    public function addParts($parts)
+    {
+        if ($parts instanceof Part) {
+            return $this->addPart($parts);
+        }
+
+        $this->parts()->saveMany($parts);
+
+        return $this;
     }
 }
